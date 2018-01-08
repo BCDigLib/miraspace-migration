6f3d618b6e1db5a9930d413797a88605061fe0b1
Adam Jazairi <adamjazairi@gmail.com>
2018-01-08 15:17:50 -0500

    Include add_manifests.js Mirador utility

---
+++ mirador-helper-scripts/add_manifests.js
@@ -0,0 +1,45 @@
+/*
+Note: this script is for testing purposes only. It might not be 
+necessary in production, since we wouldn't link users directly 
+to the Mirador landing page that lists all the objects.
+*/
+
+var fs = require('fs');
+
+var Mirador = {
+  id: "viewer",
+  data: []
+};
+
+function readLines(input, func) {
+  var remaining = '';
+
+  input.on('data', function(data) {
+    remaining += data;
+    var index = remaining.indexOf('\n');
+    while (index > -1) {
+      var line = remaining.substring(0, index);
+      remaining = remaining.substring(index + 1);
+      func(line);
+      index = remaining.indexOf('\n');
+    }
+  });
+
+  input.on('end', function() {
+    if (remaining.length > 0) {
+      func(remaining);
+    }
+  });
+}
+
+function func(data) {
+  location = {
+    manifestUri: data,
+    location: "My Repository"
+  };
+  Mirador['data'].push(location);
+}
+
+var input = fs.createReadStream('manifests/manifests.txt');
+readLines(input, func);
+console.log(Mirador);
