e4007650443b0178c4748850cdfa252fd5d4e2a2
Adam Jazairi <adamjazairi@gmail.com>
2018-02-28 16:25:15 -0500

    Add placeholders for future work

--- mirador/download_canvas.js
+++ mirador/download_canvas.js
@@ -25,49 +25,42 @@ $(function() {
   });
 
   // Largely borrowed from Harvard. Needs retooling.
-  var saveImage = function(e) {
-    $('#dl-link').click(function(e) { 
-      e.preventDefault();
-      var slot = null;
-      if (bcViewer.viewer.workspace.slots.length == 1) {
-        slot = bcViewer.viewer.workspace.slots[0];
-      } else {
-        for (var sl = 0; sl < bcViewer.viewer.workspace.slots.length; sl++) {
-          var slt = bcViewer.viewer.workspace.slots[sl];
-          if (slt.slotID == slot_idx) {
-            slot = bcViewer.viewer.workspace.slots[sl];
-            break;
-          }
-        }
-      }
-      var mirWindow = slot.window;
-      var uri = mirWindow.manifest.uri,
-          parts = uri.split("/"),
-          last_idx = parts.length - 1,
-          img_id = mirWindow.focusModules.ImageView.currentImg["label"],
-          focusType = mirWindow.currentImageMode;
+  $('#dl-link').click(function(e) { 
+    e.preventDefault();
+    var slot = null;
+    if (bcViewer.viewer.workspace.slots.length == 1) {
+      slot = bcViewer.viewer.workspace.slots[0];
+    } else {
+      // TODO: handle multiple slots
+    }
+    var mirWindow = slot.window;
+    var uri = mirWindow.manifest.uri,
+        parts = uri.split("/"),
+        last_idx = parts.length - 1,
+        img_id = mirWindow.focusModules.ImageView.currentImg["label"],
+        focusType = mirWindow.currentImageMode;
 
-      if (focusType !== "ImageView") {
-        var $error = $('#error-modal');
-        if ($error.get().length > 0) {
-          $error.dialog('close');
-        }
-        $error = $('<div id="error-modal" style="display:none" />');
-        $error.html(t['error-tmpl']({ op: "error", text: "The Save Image function is only available in single page viewing mode." }));
-        $error.appendTo('body');
-        $error.dialog($.extend({title: 'Function Unavailable'}, dialogBaseOpts)).dialog('open');
-        return;
+    if (focusType !== "ImageView") {
+      var $error = $('#error-modal');
+      if ($error.get().length > 0) {
+        $error.dialog('close');
       }
+      $error = $('<div id="error-modal" style="display:none" />');
+      $error.html(t['error-tmpl']({ op: "error", text: "The Save Image function is only available in single page viewing mode." }));
+      $error.appendTo('body');
+      $error.dialog($.extend({title: 'Function Unavailable'}, dialogBaseOpts)).dialog('open');
+      return;
+    }
 
-      var canvas = document.getElementsByName("canvas")[0], ctx = canvas.getContext("2d");
-      canvas.toBlob(function(blob) {
-        saveAs(blob, `${img_id}.jpg`)
-      });
+    var canvas = document.getElementsByName("canvas")[0];
+    canvas.toBlob(function(blob) {
+      saveAs(blob, `${img_id}.jpg`)
     });
-  }
+  });
 
+  /*
   window.addEventListener('contextmenu', function (e) { // Not compatible with IE < 9
     e.preventDefault();
-    e.saveImage();
   }, false);
+  */
 });
