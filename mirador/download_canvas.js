6a41d0fbc59a15a245afe03398c2c475c2ea9a3d
Adam Jazairi <adamjazairi@gmail.com>
2018-03-01 15:39:13 -0500

    Formatting

--- mirador/download_canvas.js
+++ mirador/download_canvas.js
@@ -22,33 +22,33 @@ var bcViewer = Mirador({
 });
 
 $(document).on('click', '#dl-link', function() {
-    var slot = null;
-    if (bcViewer.viewer.workspace.slots.length == 1) {
-      slot = bcViewer.viewer.workspace.slots[0];
-    } else {
-      // TODO: handle multiple slots
-    }
-    var mirWindow = slot.window;
-    var imgId = mirWindow.focusModules.ImageView.currentImg["label"],
-        focusType = mirWindow.currentImageMode,
-        canvasUriBase = 'http://scenery.bc.edu/',
-        canvasUriSuffix = '/full/full/0/default.jpg',
-        canvasUri = canvasUriBase + imgId + '.jp2' + canvasUriSuffix;
+  var slot = null;
+  if (bcViewer.viewer.workspace.slots.length == 1) {
+    slot = bcViewer.viewer.workspace.slots[0];
+  } else {
+    // TODO: handle multiple slots
+  }
+  var mirWindow = slot.window;
+  var imgId = mirWindow.focusModules.ImageView.currentImg["label"],
+      focusType = mirWindow.currentImageMode,
+      canvasUriBase = 'http://scenery.bc.edu/',
+      canvasUriSuffix = '/full/full/0/default.jpg',
+      canvasUri = canvasUriBase + imgId + '.jp2' + canvasUriSuffix;
 
-    if (focusType !== "ImageView") {
-      var $error = $('#error-modal');
-      if ($error.get().length > 0) {
-        $error.dialog('close');
-      }
-      $error = $('<div id="error-modal" style="display:none" />');
-      $error.html(t['error-tmpl']({ op: "error", text: "The Save Image function is only available in single page viewing mode." }));
-      $error.appendTo('body');
-      $error.dialog($.extend({title: 'Function Unavailable'}, dialogBaseOpts)).dialog('open');
-      return;
+  if (focusType !== "ImageView") {
+    var $error = $('#error-modal');
+    if ($error.get().length > 0) {
+      $error.dialog('close');
     }
+    $error = $('<div id="error-modal" style="display:none" />');
+    $error.html(t['error-tmpl']({ op: "error", text: "The Save Image function is only available in single page viewing mode." }));
+    $error.appendTo('body');
+    $error.dialog($.extend({title: 'Function Unavailable'}, dialogBaseOpts)).dialog('open');
+    return;
+  }
 
-    var a = document.getElementById("dl-link");
-    a.href = canvasUri;
+  var a = document.getElementById("dl-link");
+  a.href = canvasUri;
 });
 
 window.addEventListener('contextmenu', function (e) { // Not compatible with IE < 9
