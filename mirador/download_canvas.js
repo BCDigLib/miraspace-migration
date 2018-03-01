ce2057508dba9a3f3926d918546c48c2cbc5f558
Adam Jazairi <adamjazairi@gmail.com>
2018-03-01 15:27:04 -0500

    Refactor to remove problematic function nesting

--- mirador/download_canvas.js
+++ mirador/download_canvas.js
@@ -1,30 +1,28 @@
-var bcViewer;
-$(function() {
-  /* mdObj refers to an object declared in the corresponding HTML file that 
-  defines various viewer attributes for a given resource */
-  var l = window.mdObj;
+/* mdObj refers to an object declared in the corresponding HTML file that 
+defines various viewer attributes for a given resource */
+var l = window.mdObj;
 
-  bcViewer = Mirador({
-    "id": "viewer",
-    "mainMenuSettings": {
-      "buttons": {
-        "bookmark": false,
-        "fullScreenViewer": false
-      },
-      "userButtons": l.MIRADOR_BUTTONS,
-      "userLogo": {
-        "label": "Boston College Library",
-        "attributes": {
-          "id": "bc-logo",
-          "href": "https://library.bc.edu"
-        }
-      }
+var bcViewer = Mirador({
+  "id": "viewer",
+  "mainMenuSettings": {
+    "buttons": {
+      "bookmark": false,
+      "fullScreenViewer": false
     },
-    "data": l.MIRADOR_DATA,
-    "windowObjects": l.MIRADOR_WOBJECTS
-  });
+    "userButtons": l.MIRADOR_BUTTONS,
+    "userLogo": {
+      "label": "Boston College Library",
+      "attributes": {
+        "id": "bc-logo",
+        "href": "https://library.bc.edu"
+      }
+    }
+  },
+  "data": l.MIRADOR_DATA,
+  "windowObjects": l.MIRADOR_WOBJECTS
+});
 
-  $("#dl-link").click(function() { 
+$(document).on('click', '#dl-link', function() {
     var slot = null;
     if (bcViewer.viewer.workspace.slots.length == 1) {
       slot = bcViewer.viewer.workspace.slots[0];
@@ -52,9 +50,8 @@ $(function() {
 
     var a = document.getElementById("dl-link");
     a.href = canvasUri;
-  });
-
-  window.addEventListener('contextmenu', function (e) { // Not compatible with IE < 9
-    e.preventDefault();
-  }, false);
 });
+
+window.addEventListener('contextmenu', function (e) { // Not compatible with IE < 9
+  e.preventDefault();
+}, false);
