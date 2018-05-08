39066ad56ba4990de334ec218fb33949669aa8cf
Adam Jazairi <adamjazairi@gmail.com>
2018-05-08 11:44:49 -0400

    Update var names and add test for same handle suffix

--- bc-mirador/modify_bcimage_oai.rb
+++ bc-mirador/modify_bcimage_oai.rb
@@ -22,19 +22,24 @@ elsif !Pathname(input_xml).exist?
 end
 
 manifest = JSON.parse(File.read(input_json))
+manifest_component_unique_id = manifest['@id'].split('/').last.gsub('.json', '')
+manifest_thumb_id = manifest['thumbnail']
+manifest_handle_suffix = manifest["metadata"][0]["handle"].split('/').last
 
 doc = File.open(input_xml) { |f| Nokogiri::XML(f) }
 doc.xpath('//mods:mods', 'oai' => 'http://www.openarchives.org/OAI/2.0', 'mods' => 'http://www.loc.gov/mods/v3').each do |node|
-  component_unique_id = node.xpath('mods:identifier[@type="hdl"]', 'mods' => 'http://www.loc.gov/mods/v3').text.split('/').last
-  raw_obj = node.at_xpath('mods:location/mods:url[@access="raw object"]', 'mods' => 'http://www.loc.gov/mods/v3')
-  thumb = node.at_xpath('mods:location/mods:url[@access="preview"]', 'mods' => 'http://www.loc.gov/mods/v3')
+  oai_hdl_suffix = node.xpath('mods:identifier[@type="hdl"]', 'mods' => 'http://www.loc.gov/mods/v3').text.split('/').last
+  oai_raw_obj = node.at_xpath('mods:location/mods:url[@access="raw object"]', 'mods' => 'http://www.loc.gov/mods/v3')
+  oai_thumb = node.at_xpath('mods:location/mods:url[@access="preview"]', 'mods' => 'http://www.loc.gov/mods/v3')
 
-  raw_obj.content = "https://library.bc.edu/iiif/view/#{component_unique_id}"
-  thumb.content = "https://scenery.bc.edu/#{component_unique_id}_0001.jp2/full/!200,200/0/default.jpg"
+  if oai_hdl_suffix = manifest_handle_suffix
+    oai_raw_obj.content = "https://library.bc.edu/iiif/view/#{manifest_component_unique_id}"
+    oai_thumb.content = manifest_thumb_id
+  end
 end
 
 input_xml_rel = File.basename(input_xml.split('/').last.to_s, ".xml")
 output_xml = "#{input_xml_rel}_modded.xml"
-file = File.new(output_xml, "wb")
+file = File.new(output_xml, "w")
 file.write(doc)
 file.close
