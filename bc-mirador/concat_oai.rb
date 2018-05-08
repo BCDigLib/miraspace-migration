ed7b5255679f55070437ee24bff5d86b3443123a
Adam Jazairi <adamjazairi@gmail.com>
2018-05-08 16:47:50 -0400

    Include OAI concatenation script

---
+++ bc-mirador/concat_oai.rb
@@ -0,0 +1,27 @@
+# Concatenates multiple OAI files into a single OAI XML file
+
+require 'nokogiri'
+
+oai_dir = ARGV[0]
+input_xml = Dir["#{oai_dir}/*"]
+first_file = input_xml.shift
+
+doc = File.open(first_file) { |f| Nokogiri::XML(f) }
+doc.xpath('//oai:resumptionToken', 'oai' => 'http://www.openarchives.org/OAI/2.0/').remove
+
+records = doc.at_xpath('//oai:ListRecords', 'oai' => 'http://www.openarchives.org/OAI/2.0/')
+
+input_xml.each do |inp|
+  curr_oai = File.open(inp) { |f| Nokogiri::XML(f) }
+
+  curr_oai.xpath('//oai:record', 'oai' => 'http://www.openarchives.org/OAI/2.0/').each do |node|
+    records << node
+  end
+end
+
+doc.xpath('//oai:ListRecords', 'oai' => 'http://www.openarchives.org/OAI/2.0/').remove
+doc.at_xpath('//oai:OAI-PMH', 'oai' => 'http://www.openarchives.org/OAI/2.0/').add_child(records)
+
+output = File.new('modded_oai_combined.xml', 'w')
+output.write(doc)
+output.close
