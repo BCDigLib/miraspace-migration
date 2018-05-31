f55e1a70181cf4de4711d813ac6b3805850d5173
Adam Jazairi <adamjazairi@gmail.com>
2018-05-31 15:54:34 -0400

    Include gtag.js in view builder

--- bc-mirador/generate_mirador_view.rb
+++ bc-mirador/generate_mirador_view.rb
@@ -75,6 +75,15 @@ def build_document(file)
 <html>
 
 <head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=GA_TRACKING_ID"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'GA_TRACKING_ID');
+  </script>
   <title>#{identifier}</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
