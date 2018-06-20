26e2b77e87492a0aad26e821296f333fc6a94732
Adam Jazairi <adamjazairi@gmail.com>
2018-06-20 16:40:23 -0400

    Finish handle redirect script

--- image-and-metadata-extraction/handle-redirects/redirect_handle.rb
+++ image-and-metadata-extraction/handle-redirects/redirect_handle.rb
@@ -9,8 +9,21 @@ if ARGV.empty?
   puts "Usage: ruby redirect_handle.rb path/to/manifest.json"
 elsif File.extname(ARGV[0]) != '.json'
   puts "Input file must be a IIIF manifest JSON file"
-elsif !Pathname(ARGV[0].exist?)
+elsif !Pathname(ARGV[0]).exist?
   puts "Error: could not find #{ARGV[0]}"
 end
 
 input_file = ARGV[0]
+manifest = JSON.parse(File.read(input_file))
+handle_uri = manifest['metadata'][0]['handle']
+
+handle = handle_uri.split('/')[-2] + '/' + handle_uri.split('/')[-1]
+libsearch_prefix = 'http://bclib.bc.edu/libsearch/bc/hdl/'
+
+
+modify_statement = <<-EOF
+MODIFY #{handle}
+201 URL 86400 1110 UTF8 #{libsearch_prefix + handle}
+  EOF
+
+puts modify_statement
