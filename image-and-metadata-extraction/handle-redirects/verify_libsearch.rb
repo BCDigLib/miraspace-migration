5efdd4af53091811769375734afd45f3eb0d45f2
Adam Jazairi <16103405+jazairi@users.noreply.github.com>
2018-07-25 16:12:14 -0400

    Clean up formatting

--- image-and-metadata-extraction/handle-redirects/verify_libsearch.rb
+++ image-and-metadata-extraction/handle-redirects/verify_libsearch.rb
@@ -6,7 +6,10 @@ require 'nokogiri'
 
 input_file = ARGV[0]
 lines = File.readlines(input_file)
-urls = lines.select { |line| line.include?("URL") }.map { |line| line.gsub(/\n/, '').split(' ') }.flatten.select { |el| el.include?('http') }
+urls = lines.select { |line| line.include?("URL") }
+            .map { |el| el.gsub(/\n/, '').split(' ') }
+            .flatten
+            .select { |el| el.include?('http') }
 results = {}
 
 urls.each do |url|
