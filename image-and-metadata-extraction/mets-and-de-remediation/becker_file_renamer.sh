2231158fe8a14ed71a84aebcbee2be5d13f79acc
Adam Jazairi <16103405+jazairi@users.noreply.github.com>
2018-07-31 10:40:09 -0400

    Add script to normalize thumbnail fnames in Becker MARC

---
+++ image-and-metadata-extraction/mets-and-de-remediation/becker_file_renamer.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# This script will iterate through a Becker MARC file and normalize thumbnail filenames.
+# There are additional files that require manual editing (see becker_jp2_renaming_notes.txt
+# in collection directory for more info).
+#
+# As always, test first by removing "-i ''" from the sed commands. This will output the 
+# results to the console instead of editing the file inline.
+
+sed -i '' 's/.*__/g' becker.mrk
+sed -i '' 's/_tif*_jp2000//g' becker.mrk
